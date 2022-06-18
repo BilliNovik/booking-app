5a9bf185fb2550605fb38cf8d756e514190efca4
Viacheslav Novik <84062118+BilliNovik@users.noreply.github.com>
2022-06-18 22:30:58 +0300

    update getSearchData.js

--- client/src/servises/getSearchData.js
+++ client/src/servises/getSearchData.js
@@ -9,9 +9,9 @@ const data = {
     endDate: localStorage.getItem('date') === null ? new Date() : new Date(getItemAndParse('date')[0].endDate),
 
     // options
-    adults: localStorage.getItem('people') === null ? 2 : new Date(getItemAndParse('people').adults),
-    children: localStorage.getItem('people') === null ? 0 : new Date(getItemAndParse('people').children),
-    room: localStorage.getItem('people') === null ? 1 : new Date(getItemAndParse('people').room),
+    adults: localStorage.getItem('people') === null ? 2 : getItemAndParse('people').adults,
+    children: localStorage.getItem('people') === null ? 0 : getItemAndParse('people').children,
+    room: localStorage.getItem('people') === null ? 1 : getItemAndParse('people').room,
 }
 
 export default data
